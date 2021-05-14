6e0394f561a0f1b845d2a39efca063c37db6125f
Rohkyoungjin <rudwls468@gmail.com>
2021-05-14 19:57:34 +0900

    feat: EC2 접속 테스트

--- root/server.js
+++ root/server.js
@@ -25,7 +25,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: new MySQLStore({
-      host: "127.0.0.1",
+      host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
       post: 3306,
       user: "root",
       password: "123456",
