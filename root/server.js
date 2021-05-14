29ccdc49c93ec2670a9618a28752fe22e20fee71
Rohkyoungjin <rudwls468@gmail.com>
2021-05-14 22:53:01 +0900

    feat: EC2 테스트

--- root/server.js
+++ root/server.js
@@ -27,7 +27,7 @@ app.use(
     store: new MySQLStore({
       host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
       post: 3306,
-      user: "root",
+      user: "admin",
       password: "26958983",
       database: "project_data",
     }),
--- routes/modules/mysql.js
+++ routes/modules/mysql.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 exports.connection = mysql.createConnection({
   host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
   post: 3306,
-  user: "root",
+  user: "admin",
   password: "26958983",
   database: "project_data",
 });
