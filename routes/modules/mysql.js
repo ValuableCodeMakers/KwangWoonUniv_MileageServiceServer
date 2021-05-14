a9060f8f1d87768ca8881c5935053a7a9691a399
Rohkyoungjin <rudwls468@gmail.com>
2021-05-14 22:27:11 +0900

    Update mysql.js

--- routes/modules/mysql.js
+++ routes/modules/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 exports.connection = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
   post: 3306,
   user: "root",
   password: "123456",
