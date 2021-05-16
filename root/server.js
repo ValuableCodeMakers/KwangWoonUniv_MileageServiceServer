5282a57a6996b4d3f31a110fe486be746c0fabf1
Rohkyoungjin <rudwls468@gmail.com>
2021-05-16 20:50:21 +0900

    feat: RDS 연결중

--- root/server.js
+++ root/server.js
@@ -28,8 +28,8 @@ app.use(
       host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
       post: 3306,
       user: "admin",
-      password: "26958983",
-      database: "project_data",
+      password: "kwprojectdatabase",
+      database: "KW_project_database",
     }),
   })
 );
--- routes/modules/mysql.js
+++ routes/modules/mysql.js
@@ -4,6 +4,6 @@ exports.connection = mysql.createConnection({
   host: "kw-project-database.cvhj6cyryo94.ap-northeast-2.rds.amazonaws.com",
   post: 3306,
   user: "admin",
-  password: "26958983",
-  database: "project_data",
+  password: "kwprojectdatabase",
+  database: "KW_project_database",
 });
